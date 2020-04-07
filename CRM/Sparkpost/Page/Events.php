ad87a2d479dcd2620b911afd9302690cf9dcd512
Mathieu Lutfy <mathieu@symbiotic.coop>
2020-04-07 17:30:17 -0400

    Event delivery log: also show internal civimail errors

--- CRM/Sparkpost/Page/Events.php
+++ CRM/Sparkpost/Page/Events.php
@@ -5,22 +5,55 @@ use CRM_Sparkpost_ExtensionUtil as E;
 class CRM_Sparkpost_Page_Events extends CRM_Core_Page {
 
   public function run() {
-    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
-    CRM_Utils_System::setTitle(E::ts('Sparkpost Email Delivery Events'));
+    CRM_Utils_System::setTitle(E::ts('Email Delivery Events'));
 
     $email_id = CRM_Utils_Request::retrieveValue('email_id', 'Positive');
 
-    if ($email_id) {
-      $email = civicrm_api3('Email', 'getsingle', [
-        'id' => $email_id,
-      ])['email'];
+    if (!$email_id) {
+      throw new Exception("Missing email_id");
+    }
 
-      $events = civicrm_api3('Sparkpost', 'event', [
-        'email' => $email,
+    $email = civicrm_api3('Email', 'getsingle', [
+      'id' => $email_id,
+    ])['email'];
+
+    // Fetch Sparkpost Events
+    $events = civicrm_api3('Sparkpost', 'event', [
+      'email' => $email,
+    ]);
+
+    $this->assign('sparkpost_events', $events['values']);
+
+    // Fetch CiviCRM bounce events
+    $errors = [];
+
+    $dao = CRM_Core_DAO::executeQuery('SELECT *
+      FROM civicrm_mailing_event_bounce eb
+      LEFT JOIN civicrm_mailing_event_queue eq ON (eq.id = eb.event_queue_id)
+      WHERE email_id = %1', [
+      1 => [$email_id, 'Positive'],
+    ]);
+
+    while ($dao->fetch()) {
+      $dao2 = CRM_Core_DAO::executeQuery('SELECT j.*, m.subject, m.from_email
+        FROM civicrm_mailing_job j
+        LEFT JOIN civicrm_mailing m on (m.id = j.mailing_id)
+        WHERE j.id = %1', [
+        1 => [$dao->job_id, 'Positive'],
       ]);
 
-      $this->assign('sparkpost_events', $events['values']);
+      if ($dao2->fetch()) {
+        $errors[] = [
+          'bounce_type_id' => $dao->bounce_type_id,
+          'bounce_reason' => $dao->bounce_reason,
+          'time_stamp' => $dao->time_stamp,
+          'from' => $dao2->from_email,
+          'subject' => $dao2->subject,
+        ];
+      }
     }
+
+    $this->assign('civimail_errors', $errors);
 
     parent::run();
   }
--- templates/CRM/Sparkpost/Page/Events.tpl
+++ templates/CRM/Sparkpost/Page/Events.tpl
@@ -1,4 +1,6 @@
 {crmScope extensionKey="sparkpost"}
+  <h3>{ts}Errors from the email delivery provider{/ts}</h3>
+
   {if $sparkpost_events}
     {foreach from=$sparkpost_events item=event}
       <div style="padding-bottom: 2em;">
@@ -11,6 +13,21 @@
       </div>
     {/foreach}
   {else}
-    <p>{ts}No recent events were found for this email.{/ts}</p>
+    <p>{ts}No recent events were found for this email. Our provider only keeps a few weeks of data.{/ts}</p>
+  {/if}
+
+  <h3>{ts}CiviMail errors{/ts}</h3>
+
+  {if $civimail_errors}
+    {foreach from=$civimail_errors item=event}
+      <div style="padding-bottom: 2em;">
+        <div>{$event.time_stamp} - {$bounce_type_id.type}</div>
+        <div>{ts}From:{/ts} {$event.from}</div>
+        <div><a href="#">{$event.subject}</a></div>
+        <div>{$event.bounce_reason}</div>
+      </div>
+    {/foreach}
+  {else}
+    <p>{ts}No CiviMail errors were found for this email.{/ts}</p>
   {/if}
 {/crmScope}
