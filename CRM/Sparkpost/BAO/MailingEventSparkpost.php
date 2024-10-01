723d205fb11a848d22287eb59e0655bc784a4b2c
Samuel Vanhove <samuel@symbiotic.coop>
2024-10-01 12:18:35 -0400

    Add civicrm_mailing_event_sparkpost table to store sparkpost event

---
+++ CRM/Sparkpost/BAO/MailingEventSparkpost.php
@@ -0,0 +1,7 @@
+<?php
+
+use CRM_Sparkpost_ExtensionUtil as E;
+
+class CRM_Sparkpost_BAO_MailingEventSparkpost extends CRM_Sparkpost_DAO_MailingEventSparkpost {
+
+}
---
+++ Civi/Api4/MailingEventSparkpost.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * MailingEventSparkpost entity.
+ *
+ * Provided by the SparkPost integration extension.
+ *
+ * @package Civi\Api4
+ */
+class MailingEventSparkpost extends Generic\DAOEntity {
+
+}
